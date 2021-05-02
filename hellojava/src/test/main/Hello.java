d1690578ba71e553e2f1fad37f3699411ad4b3e0
jeonjunhyeong <jh13479@naver.com>
2021-05-02 21:25:59 +0900

    "이제 시작이군요" 출력함

--- hellojava/src/test/main/Hello.java
+++ hellojava/src/test/main/Hello.java
@@ -2,6 +2,9 @@ package test.main;
 
 public class Hello {
   public static void main(String[] args) {
-	System.out.println("hello,world");
-}
+   System.out.println("hello,world");
+	System.out.println("이제 시작이군요");
+
+  
+  }
 } 
